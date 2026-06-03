500264be3f543a3e71b9f54b93373fa9890335aa
igiehon <igiehongiedon@gmail.com>
2026-06-03 04:09:18 +0100

    Fix account flow and add legal pages

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -23,8 +23,7 @@ export function Footer() {
         <Link to="/wishlist">Wishlist</Link>
         <Link to="/cart">Cart</Link>
         <Link to="/account">Account</Link>
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-<Route path="/terms-and-conditions" element={<TermsConditions />} />
+        
       </div>
 
       <div className="footer-small">
